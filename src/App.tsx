cd88d6894c8ce1b3f4ea82caa9b4b44ed1fb9ca5
Nadia <nlnadialigia@gmail.com>
2021-06-22 21:05:10 -0300

    feat: create and style NewRoom page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 // import { Button } from './components/Button';
-import { Home } from './pages/Home';
+// import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 import './styles/global.scss';
 
 function App() {
-  return <Home />;
+  return <NewRoom />;
 }
 
 export default App;
