31c71d2ba3133283f618adc42246cdf932964f90
Nadia <nlnadialigia@gmail.com>
2021-06-22 23:43:11 -0300

    feat: create authenticate hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,22 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../App';
+import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
-import { useHistory } from 'react-router-dom';
-import firebase from 'firebase';
-import { auth } from '../services/firebase';
+import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { signInWithGoogle, user } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
 
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-
-      history.push('/rooms/new');
-    });
+    history.push('/rooms/new');
   }
 
   return (
