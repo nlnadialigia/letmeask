dac39e8035f831e82649e64b65e8cb4725e7374a
Nadia <nlnadialigia@gmail.com>
2021-06-26 16:03:28 -0300

    feat:  create likes feature

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -11,37 +11,9 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
-  // const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
-  // const [newQuestion, setNewQuestion] = useState('');
   const { questions, title } = useRoom(roomId);
-
-  // async function handleSendQuestion(event: FormEvent) {
-  //   event.preventDefault();
-
-  //   if (newQuestion.trim() === '') {
-  //     return;
-  //   }
-
-  //   if (!user) {
-  //     throw new Error('Você precisa estar logado para adicionar perguntas');
-  //   }
-
-  //   const question = {
-  //     content: newQuestion,
-  //     author: {
-  //       name: user.name,
-  //       avatar: user.avatar
-  //     },
-  //     isHighlighted: false,
-  //     isAnswered: false
-  //   };
-
-  //   await database.ref(`rooms/${roomId}/questions`).push(question);
-
-  //   setNewQuestion('');
-  // }
 
   return (
     <div id="page-room">
